88df4ddfc6a82874fa11f95c85e750884ab4f3f5
Orinameh <memefacto@yahoo.com>
2024-10-01 20:09:00 +0100

    add comments

--- app/server.py
+++ app/server.py
@@ -6,17 +6,19 @@ model = joblib.load('app/model.joblib')
 data = pd.read_csv('./final_dataset.csv')
 
 
-
+# function to predict optimal price
 def predict_optimal_price(input: pd.DataFrame):
     predicted_price = model.predict(input)
     return predicted_price[0]
 
 app = FastAPI()
 
+# Landing page of the application
 @app.get("/")
 def root():
     return {'message': 'Price optimization model deployment'}
 
+# Prediction page of the application
 @app.get("/predictions")
 def predictions():
     random = data[['Age', 'Product Category', 'Quantity', 'Demand Elasticity', 'Competitor Pricing', 'Month', 'Day of Week']].sample(n=1) 
